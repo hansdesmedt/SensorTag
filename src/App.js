bd1ca8ffd6c43d2ce34aa38b0c88d0eb993ce26c
Hans De Smedt <hans.desmedt@gmail.com>
2021-05-06 16:56:25 +0200

    added react-navigation with permission bug

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from './Store';
 import SensorTag from './SensorTag';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <SensorTag />
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        {/* Code below does not crash when bluetooth permissions were rejected in settings */}
+        {/* <SensorTag /> */}
+        {/* Code below creates a crash when bluetooth permissions were rejected in settings */}
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={SensorTag} />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
   );
 }
